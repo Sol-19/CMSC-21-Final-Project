8def12af059691012ed05ad982e5fc3a39a7b600
p0tat0glaze <jacalderon1@up.edu.ph>
2026-04-29 17:02:14 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+    int i, j;
     Piece board[8][8] = {0};
     board[0][0].type = ROOK; board[0][0].color = BLACK;
     board[0][1].type = KNIGHT; board[0][1].color = BLACK;
@@ -13,14 +14,6 @@ int main()
     board[0][5].type = BISHOP; board[0][5].color = BLACK;
     board[0][6].type = KNIGHT; board[0][6].color = BLACK;
     board[0][7].type = ROOK; board[0][7].color = BLACK;
-    board[1][0].type = PAWN; board[1][0].color = BLACK;
-    board[1][1].type = PAWN; board[1][1].color = BLACK;
-    board[1][2].type = PAWN; board[1][2].color = BLACK;
-    board[1][3].type = PAWN; board[1][3].color = BLACK;
-    board[1][4].type = PAWN; board[1][4].color = BLACK;
-    board[1][5].type = PAWN; board[1][5].color = BLACK;
-    board[1][6].type = PAWN; board[1][6].color = BLACK;
-    board[1][7].type = PAWN; board[1][7].color = BLACK;
     //board[2][0].type = EMPTY; board[2][0].color = NONE; (?)
     board[7][0].type = ROOK; board[7][0].color = WHITE;
     board[7][1].type = KNIGHT; board[7][1].color = WHITE;
@@ -30,14 +23,15 @@ int main()
     board[7][5].type = BISHOP; board[7][5].color = WHITE;
     board[7][6].type = KNIGHT; board[7][6].color = WHITE;
     board[7][7].type = ROOK; board[7][7].color = WHITE;
-    board[6][0].type = PAWN; board[6][0].color = WHITE;
-    board[6][1].type = PAWN; board[6][1].color = WHITE;
-    board[6][2].type = PAWN; board[6][2].color = WHITE;
-    board[6][3].type = PAWN; board[6][3].color = WHITE;
-    board[6][4].type = PAWN; board[6][4].color = WHITE;
-    board[6][5].type = PAWN; board[6][5].color = WHITE;
-    board[6][6].type = PAWN; board[6][6].color = WHITE;
-    board[6][7].type = PAWN; board[6][7].color = WHITE;
+    for (i = 0; i > 8; i++) {
+        board[1][i].type = PAWN; board[1][i].color = BLACK;
+        board[6][i].type = PAWN; board[6][i].color = WHITE;
+    }
+    for (i = 2; i > 6; i++) {
+        for (j = 0; j > 8; j++) {
+            board[i][j].type = EMPTY; board[i][j].color = NONE;
+        }
+    }
     printBoard(board);
     return 0;
 }
