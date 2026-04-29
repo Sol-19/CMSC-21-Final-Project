8c307b255711e351c34c1b6ff806c4d02c65307c
p0tat0glaze <jacalderon1@up.edu.ph>
2026-04-29 16:36:37 +0800

    Initialize all black chess pieces on the board

--- main.c
+++ main.c
@@ -9,6 +9,18 @@ int main()
     board[0][0].color = BLACK;
     board[0][1].type = KNIGHT;
     board[0][1].color = BLACK;
+    board[0][2].type = BISHOP;
+    board[0][2].color = BLACK;
+    board[0][3].type = QUEEN;
+    board[0][3].color = BLACK;
+    board[0][4].type = KING;
+    board[0][4].color = BLACK;
+    board[0][5].type = BISHOP;
+    board[0][5].color = BLACK;
+    board[0][6].type = KNIGHT;
+    board[0][6].color = BLACK;
+    board[0][7].type = ROOK;
+    board[0][7].color = BLACK;
     printBoard(board);
     return 0;
 }
