9c61255232779119af77404debcf630a1ae8b677
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-29 13:00:14 +0800

    fix: add BLANK enum value to PieceType for better piece representation

--- functions/chess_struct.h
+++ functions/chess_struct.h
@@ -1,6 +1,6 @@
 #ifndef CHESS_STRUCT_H
 #define CHESS_STRUCT_H
-    typedef enum{ EMPTY = 0, PAWN = 1, BISHOP = 2, KNIGHT = 3, ROOK = 4, QUEEN = 5, KING = 6} PieceType;
+    typedef enum{ EMPTY = 0, PAWN = 1, BISHOP = 2, KNIGHT = 3, ROOK = 4, QUEEN = 5, KING = 6, BLANK = 7} PieceType;
     typedef enum{ BLACK = -1, NONE = 0, WHITE = 1 } Color;
     typedef struct{
         PieceType type;
