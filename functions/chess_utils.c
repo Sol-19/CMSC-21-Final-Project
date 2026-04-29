85a12845d7beed2c69f8056bfdeb55e0ae4b0fc2
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-04-29 13:03:24 +0800

    fix: include chess_struct.h and implement printPiece function for piece representation

--- functions/chess_utils.c
+++ functions/chess_utils.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include "chess_struct.h"
 #include "chess_utils.h"
 
 
 void printBoard(void)
 {
     printf("Hi");
+}
+
+void printPiece(PieceType type, Color color) {
+    switch(type) {
+        case EMPTY:  printf("*");  break;
+        case PAWN:   printf("P");   break;
+        case BISHOP: printf("B"); break;
+        case KNIGHT: printf("N"); break;
+        case ROOK:   printf("R");   break;
+        case QUEEN:  printf("Q");  break;
+        case KING:   printf("K");   break;
+    }
 }
