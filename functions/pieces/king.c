d602a9ea74710ff3040eca6fe8ac58e7ab13f437
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-05-01 18:30:44 +0800

    refactor: simplify isLegalKing function by removing redundant switch case for turn

--- functions/pieces/king.c
+++ functions/pieces/king.c
@@ -5,50 +5,28 @@
 int isLegalKing(Piece board[8][8], Move move, Color turn)
 {
     int row = turn == WHITE?7:0;
-    switch(turn){
-        case WHITE:
-            switch(move.type){
-                case NORMAL:
-                    int row_diff = abs(move.to_x - move.from_x);
-                    int col_diff = abs(move.to_y - move.from_y);
-                    // add the logic for if king is incheck here :>
-                    return (row_diff <= 1 && col_diff <= 1);
-                case CASTLE_KINGSIDE:
-                    return( 
-                    !(board[row][4].move_count) //if king is not moved
-                    && (board[row][7].type == ROOK && board[row][7].color==WHITE && !(board[row][7].move_count))//and its a white rook and not moved
-                    && (board[row][6].type==EMPTY) // and two squares empty
-                    && (board[row][5].type==EMPTY) 
-                    //add more if not checked when going to the destination
+        switch(move.type){
+            case NORMAL:
+                int row_diff = abs(move.to_x - move.from_x);
+                int col_diff = abs(move.to_y - move.from_y);
+                // add the logic for if king is incheck here :>
+                return (row_diff <= 1 && col_diff <= 1);
+            case CASTLE_KINGSIDE:
+                return( 
+                !(board[row][4].move_count) //if king is not moved
+                && (board[row][7].type == ROOK && board[row][7].color==WHITE && !(board[row][7].move_count))//and its a white rook and not moved
+                && (board[row][6].type==EMPTY) // and two squares empty
+                && (board[row][5].type==EMPTY) 
+                //add more if not checked when going to the destination
+            );
+            case CASTLE_QUEENSIDE:
+                return (
+                !(board[row][4].move_count) //if king is not moved
+                && (board[row][0].type == ROOK && board[row][0].color==WHITE && !(board[row][0].move_count))//and its a white rook and not moved
+                && (board[row][3].type==EMPTY) // and three squares empty
+                && (board[row][2].type==EMPTY) 
+                && (board[row][1].type==EMPTY) 
+                //add more if not checked when going to the destination
                 );
-                case CASTLE_QUEENSIDE:
-                    return (
-                    !(board[row][4].move_count) //if king is not moved
-                    && (board[row][0].type == ROOK && board[row][0].color==WHITE && !(board[row][0].move_count))//and its a white rook and not moved
-                    && (board[row][3].type==EMPTY) // and three squares empty
-                    && (board[row][2].type==EMPTY) 
-                    && (board[row][1].type==EMPTY) 
-                    //add more if not checked when going to the destination
-                    );
-        }
-        case BLACK:
-            switch(move.type){
-                case NORMAL:
-                    int row_diff = abs(move.to_x - move.from_x);
-                    int col_diff = abs(move.to_y - move.from_y);
-                    // add the logic for if king is incheck here :>
-                    return (row_diff <= 1 && col_diff <= 1);
-                case CASTLE_KINGSIDE:
-                    return( 
-                    !(board[row][4].move_count) //if king is not moved
-                    && (board[row][0].type == ROOK && board[row][0].color==BLACK && !(board[row][0].move_count))//and its a black rook and not moved
-                    && (board[row][3].type==EMPTY) // and three squares empty
-                    && (board[row][2].type==EMPTY) 
-                    && (board[row][1].type==EMPTY) 
-                    //add more if not checked when going to the destination
-                );
-                case CASTLE_QUEENSIDE:
-                    return 1;
-        }
     }
 }
