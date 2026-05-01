76a87dbc5b3ca2d56c9d46eae767c8d2acaf58f9
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-05-01 18:22:25 +0800

    refactor: enhance CASTLE_KINGSIDE logic in isLegalKing function

--- functions/pieces/king.c
+++ functions/pieces/king.c
@@ -33,7 +33,14 @@ int isLegalKing(Piece board[8][8], Move move, Color turn)
                     // add the logic for if king is incheck here :>
                     return (row_diff <= 1 && col_diff <= 1);
                 case CASTLE_KINGSIDE:
-                    return 1;
+                    return( 
+                    !(board[row][4].move_count) //if king is not moved
+                    && (board[row][7].type == ROOK && board[7][7].color==BLACK)//and its a white rook
+                    && !(board[row][7].move_count) // and not moved
+                    && (board[row][6].type==EMPTY) // and two squares empty
+                    && (board[row][5].type==EMPTY) 
+                    //add more if not checked when going to the destination
+                );
                 case CASTLE_QUEENSIDE:
                     return 1;
         }
